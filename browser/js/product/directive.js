24390f47d66a5d9998603d55087d0b9f8c3cba98
Alec Friedman <alec.friedman3@gmail.com>
2016-07-29 15:00:36 -0400

    initial checkout changes

--- browser/js/product/directive.js
+++ browser/js/product/directive.js
@@ -6,14 +6,14 @@ app.directive('testProducts', function(productFactory, $state, CartFactory){
       inCartState: '='
 		},
 		templateUrl: 'js/product/index.html',
-		link: function(scope){
+		link: function (scope){
 
-			scope.deleter = function (id) {
-				productFactory.deleteById(id)
-				.then(function(res){
-					$state.reload()
-				});
-			}
+      scope.deleter = function (id) {
+        productFactory.deleteById(id)
+        .then(function(res){
+          $state.reload()
+        });
+      }
       scope.addToCart = function(id){
         var name, price;
         scope.products.forEach(function (product){
@@ -64,7 +64,7 @@ app.directive('testProducts', function(productFactory, $state, CartFactory){
         })
       }
 
-		}
+    }
 	}
 })
 
@@ -75,8 +75,119 @@ app.directive('singleProduct', function(productFactory, $state, CartFactory){
 			product: '='
 		},
 		templateUrl: 'js/product/templates/product.html',
-    link: function (scope, elem, attrs){
-      scope.addToCart = CartFactory.addToCart
+    link: function (scope){
+
+      scope.deleter = function (id) {
+        productFactory.deleteById(id)
+        .then(function(res){
+          $state.reload()
+        });
+      }
+      scope.addToCart = function(id){
+        bootbox.dialog({
+          message: '$' + scope.product.price,
+          title: 'Add ' + scope.product.firstName + ' ' + scope.product.lastName + ' to cart?',
+          buttons: {
+            success: {
+              label: 'Add to cart',
+              className: 'btn-success',
+              callback: function (){
+                console.log('worked')
+                CartFactory.addToCart(id)
+                .then(function (){
+                  $state.reload()
+                })
+              }
+            },
+            main: {
+              label: 'Express checkout',
+              className: 'btn-primary',
+              callback: function (){
+                CartFactory.addToCart(id)
+                .then(function (){
+                  $state.go('cart') //eventually make checkout
+                })
+              }
+            },
+            danger: {
+              label: 'Cancel',
+              className:'btn-danger'
+            }
+          }
+        })
+        // CartFactory.addToCart(id)
+      }
+
+
+      scope.removeFromCart = function (id){
+        CartFactory.removeFromCart(id)
+        .then(function (cart){
+          $state.reload()
+        })
+      }
+
     }
 	}
 })
+
+
+
+function productDirectiveFn(scope){
+
+      scope.deleter = function (id) {
+        productFactory.deleteById(id)
+        .then(function(res){
+          $state.reload()
+        });
+      }
+      scope.addToCart = function(id){
+        var name, price;
+        scope.products.forEach(function (product){
+          if (product.id === id){
+            name = product.firstName + ' ' + product.lastName;
+            price = product.price;
+          }
+        })
+        bootbox.dialog({
+          message: '$' + price,
+          title: 'Add ' + name + ' to cart?',
+          buttons: {
+            success: {
+              label: 'Add to cart',
+              className: 'btn-success',
+              callback: function (){
+                console.log('worked')
+                CartFactory.addToCart(id)
+                .then(function (){
+                  $state.reload()
+                })
+              }
+            },
+            main: {
+              label: 'Express checkout',
+              className: 'btn-primary',
+              callback: function (){
+                CartFactory.addToCart(id)
+                .then(function (){
+                  $state.go('cart') //eventually make checkout
+                })
+              }
+            },
+            danger: {
+              label: 'Cancel',
+              className:'btn-danger'
+            }
+          }
+        })
+        // CartFactory.addToCart(id)
+      }
+
+
+      scope.removeFromCart = function (id){
+        CartFactory.removeFromCart(id)
+        .then(function (cart){
+          $state.reload()
+        })
+      }
+
+    }
